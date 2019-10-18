431659410d3b1467ccb610e2d9a899424d68a472
cfinn16 <connorfinnegan16@gmail.com>
2019-10-17 21:05:24 -0400

    created app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    const res = axios.get(`https://api.twitter.com/1.1/friends/list.json?cursor=-1&screen_name=ConnorFinnegan`)
+    console.log(res)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello
+      </div>
+    );
+  }
 }
 
 export default App;
